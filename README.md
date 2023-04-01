b62bd1d57245e280cd8cf2f5ad3e5519ab05adc8
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-04-01 18:51:44 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unit3-02-HTML-VolumeTriangularPyramid
+[![GitHub's Super Linter](https://github.com/ICS20-Programming-EverettB/Unit3-02-HTML-VolumeTriangularPyramid/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/ICS20-Programming-EverettB/Unit3-02-HTML-VolumeTriangularPyramid/actions)
+
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Created by: Everett
+// Created on: March 2023
+// This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates total income and tax.
+ */
+function calculate () {
+  // input
+  let ValueA = parseFloat(document.getElementById('value-a').value);
+  let ValueB = parseFloat(document.getElementById('value-b').value);
+  let ValueH = parseFloat(document.getElementById('value-h').value);
+  
+  // process
+  let area = 
+  
+  // output
+  document.getElementById('area-calculate').innerHTML = 'The area is:' + area.toFixed(2)
+  
+
+}
